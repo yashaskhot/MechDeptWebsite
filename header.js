081ed571134d1f0deffc4cff7f1de6fc2ab7e55c
adityagothiwarekar <109978509+adityagothiwarekar@users.noreply.github.com>
2023-02-26 19:41:27 +0530

    added footer

---
+++ header.js
@@ -0,0 +1,98 @@
+let footer = document.getElementById('footer');
+
+
+footer.innerHTML = 
+` 
+<footer class="text-center text-lg-start text-muted" style="text-align: left; background-color: #333333;">
+<!-- Section: Social media -->
+
+<!-- Section: Social media -->
+
+<!-- Section: Links  -->
+<section class="" style="padding: 5px;">
+ <div class="container text-center text-md-start mt-5">
+   <!-- Grid row -->
+   <div class="row mt-3">
+     <!-- Grid column -->
+     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+       <!-- Content -->
+       <div class="cell-sm-6 cell-lg-3 cell-xl-4"><a rel="noopener" href="/index.html"><img class="img-responsive"
+         src="/assets/dbit-logo-inverse.webp" alt="DBIT" width="250" height="80" /></a>
+     <p class="offset-top-20 offset-md-top-35 inset-xl-right-30" style="color:white;padding: 1rem; text-align: left;"><b>Don Bosco Institute of Technology offers
+       high-quality education to students in the field of engineering. With our help, you can master any
+       sphere
+       you prefer, from computer engineering to mechanical engineering.</b></p>
+   </div>
+     </div>
+    
+     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4" style="color: 
+     #e5e5e5; margin-top: 2rem; text-align: left;">
+       <!-- Links -->
+       <h5 class="text-uppercase fw-bold mb-4"  style="text-align: center;">
+        <b>quick links</b>
+       </h5>
+       <p>
+         > <a href="https://www.dbit.in/nirf.html" class="text-reset text-school-bus-yellow-white" style="text-decoration: none;text-align: left;">NIRF</a>
+       </p>
+       <p>
+         > <a href="https://www.dbit.in/naac.html" class="text-reset" style="text-decoration: none;  text-align: left;">NAAC</a>
+       </p>
+       <p>
+         > <a href="https://www.dbit.in/nisp.html" class="text-reset" style="text-decoration: none;  text-align: left;">NISP</a>
+       </p>
+       <p>
+         > <a href="/webdevteam/contributors.html" class="text-reset" style="text-decoration: none;  text-align: left;">CONTRIBUTORS</a>
+       </p>
+     </div>
+     <!-- Grid column -->
+
+     <!-- Grid column -->
+     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4" style="color:#e5e5e5; text-align: left;
+     ">
+       <!-- Links -->
+       <p  style=" text-align: left;"><b><h5 style=" text-align: left;" >Department of Electronics & Telecommunication Engineering</h5></b><br>
+
+         1st Floor, C Wing <br>
+         
+         Don Bosco Institute Of Technology Premier Automobiles Road, Opp. Fiat Company, Kurla (W), Mumbai- 400 070 <br>
+       
+         <b>Contact :</b> 022-25042424 / 25040508 / 25042018 / 25041631 / 25042828 <br>
+
+         <b>Email -</b> dbit@dbit.in
+       </p>
+     </div>
+     <!-- Grid column -->
+     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+     <div style="padding: 5px;"></div>
+       <div class="me-5 d-none d-lg-block" style="color:white; text-align: left;">
+         <span><b>Get connected with us on social networks:</b></span>
+       </div>
+       <!-- Left -->
+       
+       <!-- Right -->
+       <div style="padding: 2px;">
+         <a  style="padding: 2px;"class="text-muted" href="https://www.facebook.com/dbitofficialfb" target="_blank"
+                  ><i class="bi bi-facebook footer-icon"  style="color:	#3b5998"></i
+               ></a>
+               <a style="padding: 2px;" class="text-muted" href="https://twitter.com/dbit_mumbai" target="_blank"
+               ><i class="bi bi-youtube footer-icon" style="color:	#FF0000"></i
+             ></a>
+             <a style="padding:2px" class="text-muted" href="https://www.instagram.com/dbitofficial_mumbai/" target="_blank"
+                 ><i class="bi bi-instagram footer-icon"  style="color:#E1306C"></i
+               ></a>
+       </div>
+     </div>
+   </div>
+   <!-- Grid row -->
+ </div>
+</section>
+<!-- Section: Links  -->
+
+<!-- Copyright -->
+<div class="text-center p-4" style="color:white" >
+ © 2021 Copyright :
+ <a class="" style="text-decoration: none; color: white;"> <a href="/webdevteam/contributors.html" style="text-decoration:none; color: white;">Desgined and Maintained by DBIT Website Maintainance Team</a></a>
+</div>
+<!-- Copyright -->
+</footer>
+`
